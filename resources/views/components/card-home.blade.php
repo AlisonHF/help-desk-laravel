9510c4bd275ce561cf60a3458733c5ccfeca07e3
Alison Faria <alisonfaria2013@gmail.com>
2026-07-30 11:30:02 -0300

    refactor(home): cria componente de card home

--- resources/views/components/card-home.blade.php
+++ resources/views/components/card-home.blade.php
@@ -1,5 +1,21 @@
-<div {{ $attributes->merge(['class' => 'card w-50 shadow-sm']) }}>
-    <div class="flex flex-row justify-center items-center p-4">
-        {{ $slot }}
+@props([
+    'icon',
+    'title',
+    'description',
+    'href' => '#',
+    'iconClass' => 'bg-blue-100 text-blue-500'
+])
+
+<div {{ $attributes->merge(['class' => 'card w-70 bg-base-100 card-sm shadow-sm']) }}>
+    <div class="card-body">
+        <div class="rounded w-min {{ $iconClass }}">
+            <x-dynamic-component :component="$icon" class="size-8 m-1" />
+        </div>
+
+        <h2 class="card-title">{{ $title }}</h2>
+        <p>{{ $description }}</p>
+        <div class="card-actions mt-1">
+            <a href="{{ $href }}" class="text-blue-700 hover:underline">Acessar →</a>
+        </div>
     </div>
 </div>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,43 +4,24 @@
 <h1 class="w-full px-5 pt-5 text-center bold text-2xl">Bem vindo {{ $user }}</h1>
 <small class="pb-5">O que faremos hoje?</small>
 <div class="flex gap-3 flex-wrap items-center justify-center">
-    <div class="card w-70 bg-base-100 card-sm shadow-sm">
-        <div class="card-body">
-            <div class="rounded bg-blue-100 w-min">
-                <x-heroicon-o-plus-circle class="size-8 m-1 text-blue-500" />
-            </div>
-            <h2 class="card-title">Abrir chamado</h2>
-            <p>Registre um novo chamado</p>
-            <div class="card-actions mt-1 hover:underline">
-                <a href="{{ route('ticket.new') }}" class="text-blue-700 ">Acessar →</a>
-            </div>
-        </div>
-    </div>
+    <x-card-home
+        icon="heroicon-o-plus-circle"
+        icon-class="bg-blue-100 text-blue-500"
+        title="Abrir chamado"
+        description="Registre um novo chamado"
+        :href="route('ticket.new')" />
 
-    <div class="card w-70 bg-base-100 card-sm shadow-sm">
-        <div class="card-body">
-            <div class="rounded bg-amber-100 w-min">
-                <x-heroicon-o-chat-bubble-bottom-center class="size-8 m-1 text-amber-500" />
-            </div>
-            <h2 class="card-title">Chamados em aberto</h2>
-            <p>Verifique os chamados em aberto</p>
-            <div class="card-actions mt-1">
-                <a href="#" class="text-blue-700 hover:underline">Acessar →</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="card w-70 bg-base-100 card-sm shadow-sm">
-        <div class="card-body">
-            <div class="rounded bg-green-100 w-min">
-                <x-heroicon-o-archive-box class="size-8 m-1 text-green-500" />
-            </div>
-            <h2 class="card-title">Histórico de chamados</h2>
-            <p>Verifique todos os seus chamados</p>
-            <div class="card-actions mt-1">
-                <a href="{{ route('ticket.list') }}" class="text-blue-700 hover:underline">Acessar →</a>
-            </div>
-        </div>
-    </div>
+    <x-card-home
+        icon="heroicon-o-chat-bubble-bottom-center"
+        icon-class="bg-amber-100 text-amber-500"
+        title="Chamados em aberto"
+        description="Verifique os chamados em aberto" />
+    
+    <x-card-home
+        icon="heroicon-o-archive-box"
+        icon-class="bg-green-100 text-green-500"
+        title="Histórico de chamados"
+        description="Verifique todos os seus chamados"
+        :href="route('ticket.list')" />
 </div>
 @endsection
