1a044ad784db8efdb90680e2e39fcacf596ca83e
Alison Faria <alisonfaria2013@gmail.com>
2026-08-03 12:30:12 -0300

    feat(ticket.list): cria cards para exibição responsiva

--- resources/views/ticket/list.blade.php
+++ resources/views/ticket/list.blade.php
@@ -1,45 +1,7 @@
 @extends('Layouts.master')
 
 @section('content')
-
-{{-- <div class="flex flex-row flex-wrap gap-4 justify-center">
-    @forelse($tickets as $ticket)
-        <div class="card w-120 bg-base-200 card-xl shadow-sm mb-3">
-            <div class="card-body">
-                <span class="badge {{ $ticket->status->color() }}">
-                    <b>{{ $ticket->status->label() }}</b>
-                </span>
-                
-                <h2 class="card-title">{{ $ticket->title }}</h2>
-
-                @can('is-technician')
-                    <small class="block mt-2">Criado por: <b>{{ $ticket->user->name }}</b></small>
-                @endcan
-                
-                <small class="block">
-                    Criado em: 
-                    <b>{{ $ticket->created_at->format('d/m/Y - h:m') }}</b>
-                </small>
-
-                <div class="card-actions">
-                    <a href="{{ route('ticket.edit', $ticket->id) }}" class="btn btn-info">Visualizar</a>
-                    
-                    @can('ticket-delete')
-                        <form method="POST" action="{{ route('ticket.delete', $ticket->id) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-error" type="submit">Excluir</button>
-                        </form>
-                    @endcan
-                </div>
-            </div>
-        </div>
-    @empty
-        <p class="text-gray-500">Nenhum chamado encontrado...</p>
-    @endforelse
-</div> --}}
-
-<div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+<div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 hidden sm:block">
     <table class="table">
         <thead>
             <tr>
@@ -79,5 +41,43 @@
             @endforeach
         </tbody>
     </table>
+</div>
+
+<div class="sm:hidden">
+    @foreach($tickets as $ticket)
+        <div class="card w-80 bg-base-100 card-xs shadow-sm mb-3">
+            <div class="card-body">
+                <div class="justify-between flex">
+                    <span class="badge {{ $ticket->status->color() }} rounded-2xl text-[10px] p-2">
+                        <b>{{ $ticket->status->label() }}</b>
+                    </span>
+                    <span class="ml-auto">{{ $ticket->created_at->format('d/m/Y H:i') }}</span>
+                </div>
+
+                <h2 class="card-title">{{ $ticket->title }}</h2>
+
+                <p class="truncate">{{ $ticket->description }}</p>
+
+                <div class="flex items-center justify-between gap-2 border-t border-base-content/10 pt-2 mt-1">
+                    <span class="min-w-0 truncate">{{ $ticket->category->description }}</span>
+
+                    <div class="flex gap-2 shrink-0">
+                        <a href="{{ route('ticket.edit', $ticket->id) }}" title="Editar" class="flex hover:underline">
+                            <x-heroicon-o-pencil-square class="size-4" />Editar
+                        </a>
+                        @can('ticket-delete')
+                            <form method="POST" action="{{ route('ticket.delete', $ticket->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button class="flex hover:cursor-pointer text-red-600 hover:underline" type="submit" title="Excluir">
+                                    <x-heroicon-o-trash class="size-4"/>Excluir
+                                </button>
+                            </form>
+                        @endcan
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
 </div>
 @endsection
